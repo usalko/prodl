614c7c8d94ab7c475309379fba4903aee4ca3005
Vanya Usalko <dk4aol@gmail.com>
2024-12-04 00:44:07 +0300

    # There is the implementation of sql_connection.

--- cmd/load.go
+++ cmd/load.go
@@ -1,32 +1,10 @@
 package cmd
 
 import (
-	"database/sql"
-	"fmt"
-	"strings"
-
 	"github.com/spf13/cobra"
-
-	_ "github.com/go-sql-driver/mysql"
-	_ "github.com/jackc/pgx/v5"
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/usalko/prodl/internal/sql_connection"
+	"github.com/usalko/prodl/internal/sql_parser/dialect"
 )
-
-func parseTargetSqlUrl(input string) (string, string, error) {
-	inputComponents := strings.SplitN(input, "//", 2)
-	driverId := strings.Trim(strings.ToLower(inputComponents[0]), ":")
-	if len(inputComponents) != 2 {
-		return driverId, "", fmt.Errorf("missed connection info in url: %v", input)
-	}
-	switch driverId {
-	case "sqlite3":
-		return driverId, "file:" + inputComponents[1], nil
-	}
-	// db, err := sql.Open("mysql", "user:password@/dbname")
-	// db, err := sql.Open("postgres", "username:password@localhost:5432/database_name")
-	// db, err := sql.Open("sqlite3", "file:locked.sqlite?cache=shared")
-	return driverId, inputComponents[1], nil
-}
 
 // loadCmd represents the load command
 var loadCmd = &cobra.Command{
@@ -37,21 +15,32 @@ var loadCmd = &cobra.Command{
 '<cmd> load --to sqlite3://./local.sqlite3 dump-file-name.tar.gz'.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		targetSqlUrl, _ := cmd.Flags().GetString("target-sql-connection")
-		driverId, connectionOptions, err := parseTargetSqlUrl(targetSqlUrl)
+		sqlDialect, connectionOptions, err := (*dialect.SqlDialect).ParseUrl(nil, targetSqlUrl)
 		if err != nil {
 			rootCmd.PrintErrf("parse target url %v fail with error: %v\n", targetSqlUrl, err)
 			return
 		}
-
-		db, err := sql.Open(driverId, connectionOptions)
+		connection, err := sql_connection.Connect(sqlDialect)
 		if err != nil {
-			rootCmd.PrintErrf("connection to the database %v fail with error: %v\n", targetSqlUrl, err)
+			rootCmd.PrintErrf("make connection structure for target url %v fail with error: %v\n", targetSqlUrl, err)
 			return
 		}
-		db.SetMaxOpenConns(1) // Sqlite3 specific
-		db.Exec("select 1")
+
+		err = connection.Establish(connectionOptions)
+		if err != nil {
+			rootCmd.PrintErrf("establish connection for target url %v fail with error: %v\n", targetSqlUrl, err)
+			return
+		}
+
+		err = connection.Execute("select 1")
+		if err != nil {
+			rootCmd.PrintErrf("check connection for target url %v fail with error: %v\n", targetSqlUrl, err)
+			return
+		}
+
 		// 1. Test connection to the database
 		// 2. Open reader and do StatementStream
+		rootCmd.Printf("connection established")
 	},
 }
 
@@ -60,7 +49,7 @@ func init() {
 Sql url for loading dump file. Examples:
 [MySQL, MariaDB, TiDB] 	mysql://user:password@/dbname
 [Sqlite3]				sqlite3://./local.sqlite3?cache=shared
-[PostgresQL]			pg://user:password@/dbname
+[PostgresQL]			pg://username:password@localhost:5432/database_name
 `)
 	rootCmd.AddCommand(loadCmd)
 }
